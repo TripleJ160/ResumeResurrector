966c9e4b25c308d2f2505c56b4ebedf6c895896b
Johan John Joji <johanusa16@gmail.com>
2024-01-09 17:13:22 -0500

    testing online funct

--- pages/2_Resume Checker.py
+++ pages/2_Resume Checker.py
@@ -3,7 +3,7 @@ import streamlit as st
 from openai import OpenAI
 import os
 #client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-client = OpenAI(api_key=st.secrets("OPENAI_API_KEY"))
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 from dotenv import load_dotenv
 import pickle
 from PyPDF2 import PdfReader
